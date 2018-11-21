0ad2a90d36b5550ceb7c3513bb4f9b7b8bf844e4
venkateshwarrao chakilam <venkateshwarrao.chakilam@FujitsuUKI.onmicrosoft.com>
2018-11-21 22:23:08 +0530

    Final sync

--- SampleWebAppDemo/Startup.cs
+++ SampleWebAppDemo/Startup.cs
@@ -27,7 +27,7 @@ namespace SampleWebAppDemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World to Azure cloud!");
             });
         }
     }
